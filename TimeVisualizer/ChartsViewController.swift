49cedfa13995feb9956a9eafcb13ffb27d0d709c
Mohammad Jaha <miro.tune@gmail.com>
2021-12-18 19:50:39 +0000

    Uploading Assignment

--- TimeVisualizer/ChartsViewController.swift
+++ TimeVisualizer/ChartsViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 import Charts
 import CoreData
 
-class ChartsViewController: UIViewController {
+class ChartsViewController: UIViewController, ChartViewDelegate {
     
     var pieChart = PieChartView()
+    var lineChart = LineChartView()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var dataList = [Times]()
+    var switchButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.automaticallyUpdatesConfiguration = false
+        button.setTitle("SWITCH", for: .normal)
+        button.layer.backgroundColor = UIColor.red.cgColor
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        return button
+    }()
+    var chartView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,24 +46,53 @@ class ChartsViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        pieChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
-        self.view.addSubview(pieChart)
-        var entries = [ChartDataEntry]()
+        chartView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        view.addSubview(chartView)
         
-        for x in 0..<9 {
-            entries.append(ChartDataEntry(x: 0, y: Double(x),data: "Hello"))
-        }
-        entries.append(ChartDataEntry(x: 0, y: 10, data: "Bye"))
-        
-        let set = PieChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.colorful()
-        let data = PieChartData(dataSet: set)
-        pieChart.data = data
-        
-        
-    }
-}
+        setUpPieChart()
+        chartView.addSubview(pieChart)
+        pieChart.isHidden = true
 
-extension ChartsViewController: ChartViewDelegate {
+        setUpLineChart()
+        chartView.addSubview(lineChart)
+        lineChart.isHidden = false
+    }
+    
+    func setUpPieChart() {
+        pieChart.frame = CGRect(x: 0, y: 0, width: self.chartView.frame.width, height: self.chartView.frame.width)
+        var entries = [PieChartDataEntry]()
+        entries.append(PieChartDataEntry(value: Double(10),label: "IOS"))
+        entries.append(PieChartDataEntry(value: Double(10),label: "algoriths"))
+        entries.append(PieChartDataEntry(value: Double(10),label: "uikit"))
+        entries.append(PieChartDataEntry(value: Double(10),label: "swiftt"))
+        entries.append(PieChartDataEntry(value: Double(10),label: "data structures"))
+        entries.append(PieChartDataEntry(value: Double(10),label: "swift ui"))
+        let dataSet = PieChartDataSet(entries: entries, label: "")
+        dataSet.colors = [.systemCyan, .blue, .purple, .systemCyan, .blue, .purple]
+        let data = PieChartData(dataSet: dataSet)
+        pieChart.data = data
+        //pieChart.chartDescription?.enabled = false
+        //pieChart.drawHoleEnabled = false //To Add Hole in the Middle
+        pieChart.rotationAngle = 0
+        pieChart.rotationEnabled = false //For Rotating The Chart
+        //pieChart.isUserInteractionEnabled = false //Used For Clicking on the chart
+        //pieChart.legend.enabled = false //Used To Display Uder Data
+        pieChart.drawSlicesUnderHoleEnabled = false
+        //pieChart.drawEntryLabelsEnabled = false //Rwiting Entry Label in slices
+        pieChart.usePercentValuesEnabled = true
+    }
+    
+    func setUpLineChart() {
+        lineChart.frame = CGRect(x: 0, y: 0, width: self.chartView.frame.width, height: self.chartView.frame.width)
+        var entries = [ChartDataEntry]()
+        entries.append(ChartDataEntry(x: 10, y: 10))
+        entries.append(ChartDataEntry(x: 20, y: 20))
+        
+        let dataSet = LineChartDataSet(entries: entries, label: "")
+        dataSet.colors = [.systemCyan, .blue, .purple, .systemCyan, .blue, .purple]
+        let data = LineChartData(dataSet: dataSet)
+        lineChart.data = data
+    }
     
 }
+
--- TimeVisualizer/ViewController.swift
+++ TimeVisualizer/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var dataList = [Times]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let date = Date()
+        let formatedDate = DateFormatter()
+        formatedDate.dateStyle = .short
+        
+        formatedDate.timeStyle = .none
+    }
+    
+    func fetchingData() {
+        let dataResult: NSFetchRequest<Times> = Times.fetchRequest()
+        do {
+            dataList = try context.fetch(dataResult)
+        }catch {
+            print(error)
+        }
     }
 
+    func saveNewData() {
+        let newData = Times(context: context)
+        newData.inDay?.inWeek?.weekNumber = 1
+        newData.inDay?.day = ""
+        newData.inDay?.date = ""
+        newData.four = ""
+        newData.fourThirty = ""
+        newData.five = ""
+        newData.fiveThirty = ""
+        newData.six = ""
+        newData.sixThirty = ""
+        newData.seven = ""
+        newData.sevenThirty = ""
+        newData.eight = ""
+        newData.eightThirty = ""
+        newData.nine = ""
+        newData.nineThirty = ""
+    }
+    
     @IBAction func addButton(_ sender: UIButton) {
         let chartsViewController = ChartsViewController()
 //        let chartsViewController = storyboard?.instantiateViewController(withIdentifier: "another") as! ChartsViewController
